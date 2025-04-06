ea16694cd1fc2d4041070a70f00772a5a36c3d09
k3l <poltorola@gmail.com>
2025-04-07 01:10:19 +0500

    find out that I didn't merge all css, .py and html files. Fixed it

--- forms.py
+++ forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Login Forms
 
 from flask_wtf import FlaskForm
@@ -8,15 +7,4 @@ from wtforms.validators import DataRequired, Email, Length
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-=======
-# Login Forms
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
->>>>>>> 560347149a73470c7a364277d95ca05008c5d080
     submit = SubmitField('Login')
